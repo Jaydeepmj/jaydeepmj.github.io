7cc3a2d65d2e49063e55235814c38dd329c4c4b2
Jaydeep Mamanpara <167412394+Jaydeepmj@users.noreply.github.com>
2025-05-21 16:17:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
+let currentLang = "en";
+
 function showSection(id) {
   document.querySelectorAll("main .content").forEach(section => {
     section.classList.remove("active");
   });
 
-  document.querySelectorAll(`main .content.lang-en`).forEach(section => {
-    if (section.id === id && currentLang === 'en') section.classList.add("active");
-  });
-
-  document.querySelectorAll(`main .content.lang-de`).forEach(section => {
-    if (section.id === id && currentLang === 'de') section.classList.add("active");
+  document.querySelectorAll(`main .content.lang-${currentLang}`).forEach(section => {
+    if (section.id === id) section.classList.add("active");
   });
 }
 
-let currentLang = "en";
 function toggleLanguage() {
   currentLang = currentLang === "en" ? "de" : "en";
   let active = document.querySelector("main .content.active");
   if (active) showSection(active.id);
+
+  // Update the toggle button text
+  const toggleButton = document.getElementById("lang-toggle");
+  toggleButton.textContent = currentLang === "en" ? "Deutsch" : "English";
 }
 
 // Show the default section on page load
-window.onload = function() {
+window.onload = function () {
   showSection("about");
 };
